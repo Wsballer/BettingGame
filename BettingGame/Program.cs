de59296e4fc00d2f5bde2bb42782a448067fbb2e
Tunji Oyewole <t.oyewole23@outlook.com>
2021-05-22 17:43:35 -0400

    Add project files.

---
+++ BettingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BettingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            Guy player = new Guy() { Cash = 100, Name = "The player" };
+
+            Console.WriteLine("Welcome to the casino. The odds are 0.75");
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.Write("How much do you want to bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = amount * 2;
+                    double houseBet = random.NextDouble();
+                    if (houseBet > odds)
+                    {
+                        Console.WriteLine("You win " + pot);
+                        player.ReceiveCash(amount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad luck, you lose.");
+                        player.GiveCash(amount);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or hit enter to exit).");
+                }
+            }
+            Console.WriteLine("The house always wins.");
+        }
+    }
+}
